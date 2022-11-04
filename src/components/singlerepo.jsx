722d9a3a95bc561f9a452956d8915566d0f2b614
Abdulhameed Busari <65597134+0xlarmideh@users.noreply.github.com>
2022-11-04 14:57:14 +0000

    Feat: SingleRepo page buildup

--- src/components/singlerepo.jsx
+++ src/components/singlerepo.jsx
@@ -5,6 +5,7 @@ const SingleRepo = () => {
  const [repos, setRepos] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const { repoId } = useParams();
+    const githubUrl = `https://github.com/0xlarmideh/${repoId}`;
     useEffect(() =>  {
     const url = `https://api.github.com/repos/0xlarmideh/${repoId}`;
     const fetchUsers = async () => {
@@ -16,12 +17,20 @@ const SingleRepo = () => {
     fetchUsers();
   }, []);
 
+  // const date = repos.created_at.slice(0,10)
+  // console.log(repos.created_at.slice(0,10))
+  let dateObj = new Date(repos.created_at);
+
+let myDate = (dateObj.getUTCFullYear()) + "/" + (dateObj.getMonth() + 1)+ "/" + (dateObj.getUTCDate());
   return isLoading ? (<img src="/src/loading.gif"></img>) : (
     <div>
       <div>
+        <p className="repo-language">{repos.language} </p>
         <h1 className="Kegilka headline repos-title"> {repos.name} </h1>
-        <p>{new Date(repos.created_at).toUTCString()} </p>
+        <p className="repo-date">{myDate}</p>
       </div>
+      <div className="repo-description">{repos.description}</div>
+      <a className="check-github" href={githubUrl}>Check on Github</a>
       <Link to="/repos">Back to repos</Link>
     </div>
   ) 
